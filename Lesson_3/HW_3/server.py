f8550aa165e07b132783cc01bbca6d17eb935cbe
zakirovrj <rjzakirov@gmail.com>
2021-03-18 16:12:54 +0100

    Lesson 4 - HW_3 solution from teacher

---
+++ Lesson_3/HW_3/server.py
@@ -0,0 +1,69 @@
+# python3 server.py 127.0.0.1 7777
+import socket
+import json
+import sys
+
+from utils.utils import load_configs, send_message, get_message
+
+CONFIGS = dict()
+
+
+def handle_message(message):
+    if CONFIGS.get('ACTION') in message \
+            and message[CONFIGS.get('ACTION')] == CONFIGS.get('PRESENCE') \
+            and CONFIGS.get('TIME') in message \
+            and CONFIGS.get('USER') in message \
+            and message[CONFIGS.get('USER')][CONFIGS.get('ACCOUNT_NAME')] == 'Guest':
+        return {CONFIGS.get('RESPONSE'): 200}
+    return {
+        CONFIGS.get('RESPONSE'): 400,
+        CONFIGS.get('ERROR'): 'Bad Request'
+    }
+
+
+def main():
+    global CONFIGS
+    CONFIGS = load_configs()
+    try:
+        if '-p' in sys.argv:
+            listen_port = int(sys.argv[sys.argv.index('-p') + 1])
+        else:
+            listen_port = CONFIGS.get('DEFAULT_PORT')
+        if not 65536 >= listen_port >= 1025:
+            raise ValueError
+    except IndexError:
+        print('After -\'p\' should be specified the port')
+        sys.exit(1)
+    except ValueError:
+        print('The port should be specified in the range from 1024 to 65535')
+        sys.exit(1)
+
+    try:
+        if '-a' in sys.argv:
+            listen_address = sys.argv[sys.argv.index('-a') + 1]
+        else:
+            listen_address = ''
+    except IndexError:
+        print('After -\'a\' - requires specify address for ')
+        sys.exit(1)
+
+    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    transport.bind((listen_address, listen_port))
+
+    transport.listen(CONFIGS.get('MAX_CONNECTIONS'))
+
+    while True:
+        client, client_address = transport.accept()
+        try:
+            message = get_message(client, CONFIGS)
+            response = handle_message(message)
+            send_message(client, response, CONFIGS)
+            # client.close()
+        except (ValueError, json.JSONDecodeError):
+            print('Received inappropriate message from a client')
+        finally:
+            client.close()
+
+
+if __name__ == '__main__':
+    main()
