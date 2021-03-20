bf110b6adf34c8e086fa7f2ac30385ad0c2e214c
zakirovrj <rjzakirov@gmail.com>
2021-03-20 14:39:02 +0100

    Lesson 5 - HW_5

---
+++ Lesson_5/HW_5/server_log_config.py
@@ -0,0 +1,13 @@
+import logging
+from logging import handlers
+
+logger = logging.getLogger('server_app')
+_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(module)s - %(message)s ')
+
+logger.addHandler(logging.handlers.TimedRotatingFileHandler('server_log.log', when='D', interval=1, delay=False))
+fh = logging.FileHandler('server_log.log', encoding='utf-8')
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(_formatter)
+
+logger.addHandler(fh)
+logger.setLevel(logging.DEBUG)
